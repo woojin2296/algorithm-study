0a670f6bbaffe5eb78b899a8c4d33cd5ab98fc6d
WooJinLim <95209789+woojin2296@users.noreply.github.com>
2024-07-22 10:57:39 +0900

    7/22 upload

---
+++ Programmers/Lv1/콜라츠 추측.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = num;
+        
+        while(n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+            }
+            else {
+                n = (n * 3) + 1;
+            }
+            
+            if (answer++ == 500) {
+                return -1;
+            }
+        }
+        
+        return answer;
+    }
+}
